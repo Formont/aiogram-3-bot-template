cfdc0e19e8f2ad4397bafcaa4b471eede7672866
Kirill <139577121+Formont@users.noreply.github.com>
2024-08-22 14:19:55 +0300

    Update models.py

--- db/models.py
+++ db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import BigInteger, FLOAT, String, TEXT
+from sqlalchemy import BigInteger, FLOAT, String, TEXT, Integer
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 
 class Base(DeclarativeBase):
@@ -8,4 +8,4 @@ class User(Base):
     __tablename__ = "users"
 
     user_id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
-    
+    active: Mapped[int] = mapped_column(Integer, default=1)
