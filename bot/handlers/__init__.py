2fdd7d4b0338cc2b121e05c1688de80e422e0037
Formont <kirillbordachev2@gmail.com>
2026-05-30 14:45:10 +0300

    Changes to be committed: 	modified:   bot/handlers/__init__.py 	modified:   config.py
    Changes not staged for commit:
    	modified:   bot/filters/adminFilter.py
    	modified:   bot/handlers/actions.py
    	modified:   config.py
    	modified:   db/orm_queries.py
    
    Untracked files:
    	.gitignore
    	requirements.txt

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,10 +1,11 @@
 from aiogram import Router
 
 def setup_routers():
-    from . import cmds
+    from . import cmds, actions
 
     router = Router()
     
     router.include_router(cmds.router)
+    router.include_router(actions.router)
 
     return router
--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TOKEN = "token"
+TOKEN = "7385943908:AAHiwp0bukk3S74UxhBKYXUJhLknYmuAQ0A"
 ADMINS = [111]
 DATABASE_URL = "sqlite+aiosqlite:///database.db"
